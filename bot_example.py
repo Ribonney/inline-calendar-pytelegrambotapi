78d8983d5800a31e8d41ff332e3c8f40dd6da62f
David Dobryakov <32088636+kantegory@users.noreply.github.com>
2019-08-14 22:08:43 +0300

    Update bot_example.py
    telegram calendar bot with pytelegrambotapi (https://github.com/eternnoir/pyTelegramBotAPI)

--- bot_example.py
+++ bot_example.py
@@ -1,44 +1,83 @@
-#!/usr/bin/python3
+import telebot
+import datetime
+from telegramcalendar import create_calendar
 
-import logging
-
-from telegram.ext import Updater
-from telegram.ext import  CallbackQueryHandler
-from telegram.ext import  CommandHandler
-from telegram import  ReplyKeyboardRemove
-
-
-import telegramcalendar
+telebot.apihelper.proxy = {'https': 'socks5://userproxy:password@ip:port'}
+bot = telebot.TeleBot("YOUR-TOKEN")
+current_shown_dates={}
 
 
-TOKEN = ""
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Current date
+    chat_id = message.chat.id
+    date = (now.year,now.month)
+    current_shown_dates[chat_id] = date #Saving the current date in a dict
+
+    markup = create_calendar(now.year, now.month)
+    bot.send_message(message.chat.id, "Please, choose a date", reply_markup=markup)
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+@bot.callback_query_handler(func=lambda call: 'DAY' in call.data[0:13])
+def get_day(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
 
-logger = logging.getLogger(__name__)
+    if (saved_date is not None):
+        day=call.data[11:]
+        print(day)
+        date = datetime.datetime(int(saved_date[0]),int(saved_date[1]),int(day),0,0,0)
+        bot.send_message(chat_id, str(date))
+        bot.answer_callback_query(call.id, text="")
 
-def calendar_handler(bot,update):
-    update.message.reply_text("Please select a date: ",
-                        reply_markup=telegramcalendar.create_calendar())
-
-
-def inline_handler(bot,update):
-    selected,date = telegramcalendar.process_calendar_selection(bot, update)
-    if selected:
-        bot.send_message(chat_id=update.callback_query.from_user.id,
-                        text="You selected %s" % (date.strftime("%d/%m/%Y")),
-                        reply_markup=ReplyKeyboardRemove())
+    else:
+        # add your reaction for shown an error
+        pass
 
 
-if TOKEN == "":
-    print("Please write TOKEN into file")
-else:
-    up = Updater("TOKEN")
+@bot.callback_query_handler(func=lambda call: 'NEXT-MONTH' in call.data)
+def next_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if (saved_date is not None):
+        year,month = saved_date
+        month+=1
+        if month>12:
+            month=1
+            year+=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup = create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        # add your reaction for shown an error
+        pass
 
-    up.dispatcher.add_handler(CommandHandler("calendar",calendar_handler))
-    up.dispatcher.add_handler(CallbackQueryHandler(inline_handler))
 
-    up.start_polling()
-    up.idle()
+@bot.callback_query_handler(func=lambda call: 'PREV-MONTH' in call.data)
+def previous_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month-=1
+        if month<1:
+            month=12
+            year-=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        # add your reaction for shown an error
+        pass
+
+
+@bot.callback_query_handler(func=lambda call: "IGNORE" in call.data)
+def ignore(call):
+    bot.answer_callback_query(call.id, text="smth wrong")
+
+
+bot.polling()
