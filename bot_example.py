4ddf3432af8c01e3026d9f9018f4f132bebb2d48
David Dobryakov <32088636+kantegory@users.noreply.github.com>
2019-10-05 18:10:34 +0300

    Update bot_example.py
    concatenate prev-/next-month functions, optimize imports

--- bot_example.py
+++ bot_example.py
@@ -1,21 +1,22 @@
 import telebot
 import datetime
-from telegramcalendar import create_calendar
-from telebot import types
+from utils.telegramcalendar import create_calendar
 
 telebot.apihelper.proxy = {'https': 'socks5://userproxy:password@ip:port'}
 bot = telebot.TeleBot("YOUR-TOKEN")
 current_shown_dates={}
 
-
 @bot.message_handler(commands=['calendar'])
 def get_calendar(message):
-    now = datetime.datetime.now() #Current date
+
+    now = datetime.datetime.now()
     chat_id = message.chat.id
-    date = (now.year,now.month)
-    current_shown_dates[chat_id] = date #Saving the current date in a dict
+
+    date = (now.year, now.month)
+    current_shown_dates[chat_id] = date
 
     markup = create_calendar(now.year, now.month)
+
     bot.send_message(message.chat.id, "Please, choose a date", reply_markup=markup)
 
 
@@ -23,12 +24,13 @@ def get_calendar(message):
 def get_day(call):
     chat_id = call.message.chat.id
     saved_date = current_shown_dates.get(chat_id)
+    last_sep = call.data.rfind(';') + 1
 
-    if (saved_date is not None):
-        day=call.data[11:]
-        print(day)
-        date = datetime.datetime(int(saved_date[0]),int(saved_date[1]),int(day),0,0,0)
-        bot.send_message(chat_id, str(date))
+    if saved_date is not None:
+
+        day = call.data[last_sep:]
+        date = datetime.datetime(int(saved_date[0]), int(saved_date[1]), int(day), 0, 0, 0)
+        bot.send_message(chat_id=chat_id, text=str(date))
         bot.answer_callback_query(call.id, text="")
 
     else:
@@ -36,49 +38,38 @@ def get_day(call):
         pass
 
 
-@bot.callback_query_handler(func=lambda call: 'NEXT-MONTH' in call.data)
-def next_month(call):
+@bot.callback_query_handler(func=lambda call: 'MONTH' in call.data)
+def handle_month_query(call):
+
+    info = call.data.split(';')
+    month_opt = info[0].split('-')[0]
+    year, month = int(info[1]), int(info[2])
     chat_id = call.message.chat.id
-    saved_date = current_shown_dates.get(chat_id)
-    if (saved_date is not None):
-        year,month = saved_date
-        month+=1
-        if month>12:
-            month=1
-            year+=1
-        date = (year,month)
-        current_shown_dates[chat_id] = date
-        markup = create_calendar(year,month)
-        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
-        bot.answer_callback_query(call.id, text="")
-    else:
-        # add your reaction for shown an error
-        pass
 
+    if month_opt == 'PREV':
+        month -= 1
 
-@bot.callback_query_handler(func=lambda call: 'PREV-MONTH' in call.data)
-def previous_month(call):
-    chat_id = call.message.chat.id
-    saved_date = current_shown_dates.get(chat_id)
-    if(saved_date is not None):
-        year,month = saved_date
-        month-=1
-        if month<1:
-            month=12
-            year-=1
-        date = (year,month)
-        current_shown_dates[chat_id] = date
-        markup= create_calendar(year,month)
-        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
-        bot.answer_callback_query(call.id, text="")
-    else:
-        # add your reaction for shown an error
-        pass
+    elif month_opt == 'NEXT':
+        month += 1
+
+    if month < 1:
+        month = 12
+        year -= 1
+
+    if month > 12:
+        month = 1
+        year += 1
+
+    date = (year, month)
+    current_shown_dates[chat_id] = date
+    markup = create_calendar(year, month)
+    bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: "IGNORE" in call.data)
 def ignore(call):
-    bot.answer_callback_query(call.id, text="smth wrong")
+    bot.answer_callback_query(call.id, text="OOPS... something went wrong")
 
 
-bot.polling()
+if __name__ == "__main__":
+    bot.polling()
